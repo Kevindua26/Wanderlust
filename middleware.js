0444321d4c4d87fc85df85fe362b9187d3120435
Kevindua26 <kevindua26@gmail.com>
2025-07-25 00:20:27 +0530

    Bug Fix

--- middleware.js
+++ middleware.js
@@ -23,7 +23,7 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 module.exports.isOwner = async(req, res, next) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
-  if (!listing.owner.equals(currentUser._id)) {
+  if (!listing.owner.equals(res.locals.currentUser._id)) {
     req.flash("error", "You are not authorized to do that.");
     return res.redirect(`/listings/${id}`);
   }
@@ -33,7 +33,7 @@ module.exports.isOwner = async(req, res, next) => {
 module.exports.isReviewAuthor = async(req, res, next) => {
   let { id, reviewId } = req.params;
   let review = await Review.findById(reviewId);
-  if (!review.author.equals(currentUser._id)) {
+  if (!review.author.equals(res.locals.currentUser._id)) {
     req.flash("error", "You are not authorized to do that.");
     return res.redirect(`/listings/${id}#comment-section`);
   }
